a675b0d1b5cdb20bfd749597443d4db8fbe17333
Max <139768623+zerotul777@users.noreply.github.com>
2024-10-26 16:45:28 +0300

    demo

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 const burgerBtn = document.querySelector('.burger');
 const closeBtn = document.querySelector('.modal__close');
-const hero = document.querySelector('.hero');
+const header = document.querySelector('.header');
 
 burgerBtn.addEventListener('click', () => {
-	hero.classList.toggle('hero--open');
+	header.classList.toggle('header--open');
 });
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
-	const projectCards = document.querySelectorAll('.project-card');
-	const modal = document.querySelector('.modal');
-	const closeBtn = document.querySelector('.modal__close');
+// document.addEventListener('DOMContentLoaded', () => {
+// 	const projectCards = document.querySelectorAll('.project-card');
+// 	const modal = document.querySelector('.modal');
+// 	const closeBtn = document.querySelector('.modal__close');
 
-	closeBtn.addEventListener('click', () => modal.classList.remove('modal--open'));
+// 	closeBtn.addEventListener('click', () => modal.classList.remove('modal--open'));
 
-	projectCards.forEach(card => {
-		card.addEventListener('click', () => {
-			const targetId = card.getAttribute('data-modal-id').slice(1);
-			document.querySelectorAll('.modal__item').forEach(item => item.classList.remove('modal__item--open'));
+// 	projectCards.forEach(card => {
+// 		card.addEventListener('click', () => {
+// 			const targetId = card.getAttribute('data-modal-id').slice(1);
+// 			document.querySelectorAll('.modal__item').forEach(item => item.classList.remove('modal__item--open'));
 			
-			modal.classList.add('modal--open');
-			const targetItem = document.getElementById(targetId);
-			if (targetItem) targetItem.classList.add('modal__item--open');
-		});
-	});
-});
+// 			modal.classList.add('modal--open');
+// 			const targetItem = document.getElementById(targetId);
+// 			if (targetItem) targetItem.classList.add('modal__item--open');
+// 		});
+// 	});
+// });
 
-// Swiper
-const swiper = new Swiper('.swiper', {
-	direction: 'horizontal',
-	loop: true,
-	centeredSlides: true,
-	pagination: {
-		el: '.swiper-pagination',
-	},
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	}
-});
+// // Swiper
+// const swiper = new Swiper('.swiper', {
+// 	direction: 'horizontal',
+// 	loop: true,
+// 	centeredSlides: true,
+// 	pagination: {
+// 		el: '.swiper-pagination',
+// 	},
+// 	navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev',
+// 	}
+// });
