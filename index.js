85d47dd5fb65291701b917a61dd1730938c6ec81
Max <139768623+zerotul777@users.noreply.github.com>
2024-10-27 15:43:57 +0300

    demo

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const burgerBtn = document.querySelector('.burger');
 const closeBtn = document.querySelector('.modal__close');
 const header = document.querySelector('.header');
+const headerLinks = document.querySelectorAll('.header__link');
 
 burgerBtn.addEventListener('click', () => {
 	header.classList.toggle('header--open');
 });
 
+headerLinks.forEach((link) => {
+	link.addEventListener('click', () => {
+		header.classList.remove('header--open');
+	});
+});
 
+const heroBg = document.querySelector('.hero__img');
+window.addEventListener('scroll', () => {
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+	// Получаем максимальное значение скролла
+	const maxScroll = scrollHeight - clientHeight;
+
+	// Вычисляем масштаб
+	const power = 1.2;
+	const scale = 1 + (scrollTop / maxScroll) * power;
+
+	// Устанавливаем масштаб
+	heroBg.style.transform = `scale(${scale})`;
+});
 
 // document.addEventListener('DOMContentLoaded', () => {
 // 	const projectCards = document.querySelectorAll('.project-card');
@@ -19,7 +39,7 @@ burgerBtn.addEventListener('click', () => {
 // 		card.addEventListener('click', () => {
 // 			const targetId = card.getAttribute('data-modal-id').slice(1);
 // 			document.querySelectorAll('.modal__item').forEach(item => item.classList.remove('modal__item--open'));
-			
+
 // 			modal.classList.add('modal--open');
 // 			const targetItem = document.getElementById(targetId);
 // 			if (targetItem) targetItem.classList.add('modal__item--open');
